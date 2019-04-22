64b8200589f0f3b00ea49bf7efdd4ea2853ab26f
Henrik Rydgård <hrydgard@gmail.com>
2019-04-22 17:16:35 +0200

    Increase buffer size. Fixes #16, thanks afbjorklund

--- minitrace.c
+++ minitrace.c
@@ -239,10 +239,9 @@ void mtr_flush() {
 #endif
 	int i = 0;
 	char linebuf[1024];
-	char arg_buf[256];
+	char arg_buf[1024];
 	char id_buf[256];
 	// We have to lock while flushing. So we really should avoid flushing as much as possible.
-
 
 	pthread_mutex_lock(&mutex);
 	int old_tracing = is_tracing;
